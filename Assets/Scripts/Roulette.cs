a3bedb315d42a7a66ed09850774e207d54a63539
keisuke <88765310+melon0658@users.noreply.github.com>
2022-09-19 21:06:57 +0900

    マップ配置と移動処理

---
+++ Assets/Scripts/Roulette.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Roulette : MonoBehaviour
+{
+    public GameObject roulette;
+    public Slider slider;
+    public Button button;
+    private bool maxValue;
+    private bool isClicked;
+    private bool rotate;
+    private float speed;
+    private float slowDownSpeed = 0.99f;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = 0;
+        maxValue = false;
+        isClicked = false;
+        rotate = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //クリックされていなければ実行
+        if (!isClicked)
+        {
+            //5に達した場合と、0に戻った場合のフラグ切替え
+            if (slider.value == 1)
+            {
+                maxValue = true;
+            }
+
+            if (slider.value == 0)
+            {
+                maxValue = false;
+            }
+
+            //フラグによるスライダー値の増減
+            if (maxValue)
+            {
+                slider.value -= 0.005f;
+            }
+            else
+            {
+                slider.value += 0.005f;
+            }
+            //Debug.Log(slider.value);
+        }
+
+        if (rotate)
+        {
+            roulette.transform.Rotate(0, speed, 0);
+            speed *= slowDownSpeed;
+            slowDownSpeed -= 0.001f * Time.deltaTime;
+
+            if (speed < 0.005f)
+            {
+                rotate = false;
+                count();
+            }
+        }
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("stop!!!!!!!!!!!!");
+        isClicked = true;
+        rotate = true;
+        speed = (1 - slider.value) * 40;
+    }
+
+    public void count()
+    {
+        int ang;
+        if(roulette.transform.localEulerAngles.y > 0)
+        {
+            ang = (int)roulette.transform.localEulerAngles.y;
+        }
+        else
+        {
+            ang = (int)(-1.0 * roulette.transform.localEulerAngles.y + 180);
+        }
+        ang = 10 - ang / 36;
+        Text tt = GameObject.Find("Text(Legacy)").GetComponent<Text>();
+        tt.text = ang.ToString();
+    }
+
+}
