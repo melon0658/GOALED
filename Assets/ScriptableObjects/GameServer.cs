18ffa6d28be8cb4a03c737313b2da257620ac752
shima004 <yellow030408@gmail.com>
2022-10-12 12:08:34 +0900

    add localmode

--- Assets/ScriptableObjects/GameServer.cs
+++ Assets/ScriptableObjects/GameServer.cs
@@ -5,14 +5,24 @@ using Grpc.Core;
 [CreateAssetMenu(fileName = "GameServer", menuName = "GameServer", order = 1)]
 public class GameServer : ScriptableObject, ISerializationCallbackReceiver
 {
+  [SerializeField] private string localHost;
+  [SerializeField] private int localPort;
   [SerializeField] private string host;
   [SerializeField] private int port;
+  [SerializeField] private bool isLocal;
   [NonSerialized] public Channel channel;
   [NonSerialized] public GameService.GameService.GameServiceClient client;
 
   public void OnEnable()
   {
-    channel = new Channel(host, port, ChannelCredentials.Insecure);
+    if (isLocal)
+    {
+      channel = new Channel(localHost, localPort, ChannelCredentials.Insecure);
+    }
+    else
+    {
+      channel = new Channel(host, port, ChannelCredentials.Insecure);
+    }
     client = new GameService.GameService.GameServiceClient(channel);
   }
 
