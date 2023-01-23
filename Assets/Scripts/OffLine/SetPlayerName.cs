e1d59c496aea99082e7b0c6911afad7c2903101d
keisuke <88765310+melon0658@users.noreply.github.com>
2023-01-24 04:36:06 +0900

    プレイヤー名セットするスクリプトを追加(オフライン用)

---
+++ Assets/Scripts/OffLine/SetPlayerName.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SetPlayerName : MonoBehaviour
+{
+  private string playerName1;
+  private string playerName2;
+  private string playerName3;
+  private string playerName4;
+  private string[] playerNames;
+
+  public string[] GetPlayerNames()
+  {
+    return playerNames;
+  }
+
+  void Start()
+  {
+    playerNames = new string[] { playerName1, playerName2, playerName3, playerName4 };
+
+    for(int i = 0; i < 4; i++)
+    {
+      playerNames[i] = "";
+    }
+  }
+
+  public void SetPlayerName1(string text)
+  {
+
+    playerNames[0] = text;
+  }
+
+
+  public void SetPlayerName2(string text)
+  {
+
+    playerNames[1] = text;
+  }
+
+
+  public void SetPlayerName3(string text)
+  {
+
+    playerNames[2] = text;
+  }
+
+
+  public void SetPlayerName4(string text)
+  {
+
+    playerNames[3] = text;
+  }
+}
